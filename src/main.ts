8372c8c4a72a103ebb35d97bcaefd8f172feebad
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-05-06 22:46:33 +0300

    Заливка

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app
+    .listen(port)
+    .then(() => {
+      console.log(`🚀 Сервер запущен по адресу: http://localhost:${port}/${globalPrefix}`);
+    })
+    .catch((err) => {
+      console.error(`Сервер не запустился по ошибке: ${err}`);
+    });
+}
+
+bootstrap().catch(console.error);
